a35a389d0caeac3b3e0a89ded7161ffc5e8acd02
viktorb8 <victor.bitencourt8@gmail.com>
2022-06-06 15:38:01 -0400

    python decouple

--- Gerenciador_Projetos/settings.py
+++ Gerenciador_Projetos/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_on_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '^@6fi(bgw$3$v40eyxgr-=q7c!vs23t)b#5a8ml6%(pj1sa4ft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
